9925d88254e913e8b028bc3a7e829057c7d08ff7
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-06-10 17:08:33 +0200

    MAINT: various fixes in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+# -*- coding: utf8 -*-
+from setuptools import setup
 from distutils.extension import Extension
 
 module = 'stockholm_reformat'
@@ -11,12 +12,20 @@ setup(name=module, version='0.1',
       ext_modules=[ext],
       description='Fast Stockholm to A3M Multiple Sequence Alignment reformater',
       url='https://github.com/Dapid/stockholm_reformat',
-      author='David Men\'endez Hurtado',
+      author='David Menéndez Hurtado',
       author_email='davidmenhur@gmail.com',
       license='BSD 3-clause',
       packages=['stockholm_reformat'],
       test_suite='nose.collector',
       tests_require=['nose'],
       scripts=['bin/stockholm_to_a3m'],
-      zip_safe=True
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Cython',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Intended Audience :: Science/Research',
+                   'Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: BSD License']
     )
