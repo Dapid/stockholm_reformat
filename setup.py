35e55436c9cfa9dbba5f151a0b3f76e2254c131d
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-06-10 16:14:29 +0200

    MAINT: Cython version now compiles

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 module = 'stockholm_reformat'
-ext = Extension(module, ['stockholm_reformat/creformat.c'],
+ext = Extension('stockholm_reformat.creformat',
+                sources=['stockholm_reformat/creformat.c'],
                 extra_compile_args=['-O2', '-mtune=native', '-funroll-loops',
                                     '-fpic', '-flto', '-g0'])
 
 setup(name=module, version='0.1',
+      ext_modules=[ext],
       description='Stockholm to A3M multiple sequence alignment reformater',
       url='https://github.com/Dapid/stockholm_reformat',
       author='David Men\'endez Hurtado',
       license='BSD 3-clause',
       packages=['stockholm_reformat'],
       test_suite='nose.collector',
-      tests_require=['nose'],
-      ext_modules = [ext]
+      tests_require=['nose']
     )
--- stockholm_reformat/__init__.py
+++ stockholm_reformat/__init__.py
@@ -3,3 +3,4 @@ try:
     from creformat import cparse
 except ImportError:
     pass
+__version__ = 0.1
