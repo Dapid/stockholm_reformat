0a741b270d96cb91df29cf6e64488798dcb7bdac
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-08-19 16:25:51 +0200

    TST: more extensive and thorough testing

--- stockholm_reformat/__init__.py
+++ stockholm_reformat/__init__.py
@@ -1,6 +1,6 @@
-from reformat import parse_a3m, parse_fasta
+from reformat import parse_a3m, parse_fasta, parse_aln
 try:
-    from creformat import cparse_a3m, cparse_fasta
+    from creformat import cparse_a3m, cparse_fasta, cparse_aln
 except ImportError:
     pass
 __version__ = '0.2.dev1'
