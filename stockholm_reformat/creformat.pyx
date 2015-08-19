05c6abdcc0a1dcd4cd871503130a0c364d36072f
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-08-19 16:04:10 +0200

    ENH: including aln formatter

--- stockholm_reformat/creformat.pyx
+++ stockholm_reformat/creformat.pyx
@@ -61,6 +61,7 @@ def cparse_a3m(inputfile, outputfile):
         output.write('\n')
     output.close()
 
+
 def cparse_fasta(inputfile, outputfile):
     # Pre-load the file in memory, if vmtouch is installed.
     os.system('vmtouch -qt {} & >/dev/null 2>/dev/null'.format(inputfile))
@@ -117,6 +118,65 @@ def cparse_fasta(inputfile, outputfile):
 
     for name, seq_l in data.iteritems():
         output.write(''.join(('>', name, '\n')))
+        output.write(''.join(seq_l))
+        output.write('\n')
+    output.close()
+
+
+def cparse_aln(inputfile, outputfile):
+    # Pre-load the file in memory, if vmtouch is installed.
+    os.system('vmtouch -qt {} & >/dev/null 2>/dev/null'.format(inputfile))
+    cdef str line, header, sequence, name, seq
+    cdef str s
+    cdef short int i
+    cdef size_t j
+    cdef list data_name
+
+    input_ = open(inputfile)
+    output = open(outputfile, 'w')
+
+    data = defaultdict(list)
+    reference_seq = []
+
+    for line in input_:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            header, sequence = line.split()
+            break
+
+    while True:
+        reference_seq.extend((s for s in sequence if s != '-'))
+        index = [1 if s != '-' else 0 for s in sequence]
+        for line in input_:
+            line = line.strip()
+            if not line:
+                break
+            if not line.startswith('#'):
+                try:
+                    name, seq = line.split()
+                    data_name = data[name]
+                    for j in range(len(seq)):
+                        i = index[j]
+                        if i:
+                            s = seq[j]
+                            data_name.append(s)
+                except ValueError:
+                    # End of file
+                    pass
+
+        try:
+            header, sequence = input_.next().split()
+        except StopIteration:
+            break
+
+    input_.close()
+    os.system('vmtouch -qe {} & >/dev/null 2>/dev/null'.format(inputfile))
+
+    # Write to file
+    output.write(''.join(reference_seq))
+    output.write('\n')
+
+    for name, seq_l in data.iteritems():
         output.write(''.join(seq_l))
         output.write('\n')
     output.close()
--- stockholm_reformat/reformat.py
+++ stockholm_reformat/reformat.py
@@ -2,13 +2,14 @@ import os
 from collections import defaultdict
 
 
-def parse_a3m(inputfile, outputfile):
-    # Pre-load the file in memory, if vmtouch is installed.
-    os.system('vmtouch -qt {} & >/dev/null 2>/dev/null'.format(inputfile))
-
+def _validate_input(inputfile, outputfile):
     if isinstance(inputfile, basestring):
+        # Pre-load the file in memory, if vmtouch is installed.
+        os.system('vmtouch -qt {} & >/dev/null 2>/dev/null'.format(inputfile))
+
         input_ = open(inputfile)
         close_input = True
+
     elif isinstance(inputfile, file):
         input_ = inputfile
         close_input = False
@@ -25,6 +26,12 @@ def parse_a3m(inputfile, outputfile):
     else:
         raise ValueError('outputfile should be a file name or a'
                          'file handler, got {} instead.'.format(type(outputfile)))
+
+    return input_, close_input, output, close_output
+
+
+def parse_a3m(inputfile, outputfile):
+    input_, close_input, output, close_output = _validate_input(inputfile, outputfile)
 
     data = defaultdict(list)
     reference_seq = []
@@ -74,29 +81,9 @@ def parse_a3m(inputfile, outputfile):
     else:
         output.flush()
 
+
 def parse_fasta(inputfile, outputfile):
-    # Pre-load the file in memory, if vmtouch is installed.
-    os.system('vmtouch -qt {} & >/dev/null 2>/dev/null'.format(inputfile))
-
-    if isinstance(inputfile, basestring):
-        input_ = open(inputfile)
-        close_input = True
-    elif isinstance(inputfile, file):
-        input_ = inputfile
-        close_input = False
-    else:
-        raise ValueError('inputfile should be a file name or a'
-                         'file handler, got {} instead.'.format(type(inputfile)))
-
-    if isinstance(outputfile, basestring):
-        output = open(outputfile, 'w')
-        close_output = True
-    elif isinstance(outputfile, file):
-        output = outputfile
-        close_output = False
-    else:
-        raise ValueError('outputfile should be a file name or a'
-                         'file handler, got {} instead.'.format(type(outputfile)))
+    input_, close_input, output, close_output = _validate_input(inputfile, outputfile)
 
     data = defaultdict(list)
     reference_seq = []
@@ -138,6 +125,57 @@ def parse_fasta(inputfile, outputfile):
 
     for name, seq in data.iteritems():
         output.write(''.join(('>', name, '\n')))
+        output.write(''.join(seq))
+        output.write('\n')
+
+    if close_output:
+        output.close()
+    else:
+        output.flush()
+
+
+def parse_aln(inputfile, outputfile):
+    # This is as parse_fasta, but without printing the headers in the output.
+    input_, close_input, output, close_output = _validate_input(inputfile, outputfile)
+
+    data = defaultdict(list)
+    reference_seq = []
+
+    for line in input_:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            header, sequence = line.split()
+            break
+
+    while True:
+        reference_seq.extend((s for s in sequence if s != '-'))
+        index = [True if s != '-' else False for s in sequence]
+        for line in input_:
+            line = line.strip()
+            if not line:
+                break
+            if not line.startswith('#'):
+                try:
+                    name, seq = line.split()
+                    data[name].extend(s for s, i in zip(seq, index) if i)
+                except ValueError:
+                    # End of file
+                    pass
+
+        try:
+            header, sequence = input_.next().split()
+        except StopIteration:
+            break
+
+    if close_input:
+        input_.close()
+    os.system('vmtouch -qe {} & >/dev/null 2>/dev/null'.format(inputfile))
+
+    # Write to file
+    output.write(''.join(reference_seq))
+    output.write('\n')
+
+    for name, seq in data.iteritems():
         output.write(''.join(seq))
         output.write('\n')
 
