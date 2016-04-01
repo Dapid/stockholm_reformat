a323b782bebb11b3e94afda2be52ddb6b67ff4b8
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2016-04-01 14:56:21 +0200

    ENH: script to profile

--- stockholm_reformat/tests/profile.py
+++ stockholm_reformat/tests/profile.py
@@ -1,123 +1,32 @@
 import os
-from nose.exc import SkipTest
 
 from stockholm_reformat import parse_a3m, parse_fasta, parse_aln
-
-def assert_equal_a3m(ref_file, check_file):
-    new = dict()
-    ref = dict()
-
-    check = open(check_file)
-    for line in check:
-        new[line.strip()] = check.next().strip()
-    check.close()
-
-    _this = []
-    _head = None
-    for line in open(ref_file):
-        if line.startswith('>'):
-            sequence = ''.join(_this)
-            ref[_head] = sequence
-
-            _head = line.strip()
-            _this = []
-        else:
-            _this.append(line.strip())
-    ref[_head] = ''.join(_this)
-    del ref[None]
-
-    for ky in ref.keys():
-        assert ref[ky] == new[ky], (ref[ky], new[ky])
-
-    assert len(ref) == len(new)
+from stockholm_reformat import cparse_a3m, cparse_fasta, cparse_aln
 
 
-def assert_equal_files(one, other):
-    one_f = open(one)
-    other_f = open(other)
-    for line1, line2 in zip(one_f, other_f):
-        if not line1 == line2:
-            print line1
-            print line2
-            raise AssertionError
-    # Check we are at the end of the file:
-    assert one_f.read() == ''
-    assert other_f.read() == ''
-
-
-def test_purepython_a3m():
-    parse_a3m('data/sequence.fa.sto', 'data/output.a3m')
-    assert_equal_a3m('data/sequence.fa.a3m','data/output.a3m')
-    os.unlink('data/output.a3m')
-
-
-def test_cython_a3m():
-    try:
-        from stockholm_reformat import cparse_a3m
-    except ImportError:
-        raise SkipTest('Compiled version unavailable')
-
-    cparse_a3m('data/sequence.fa.sto', 'data/output_c.a3m')
-    assert_equal_a3m('data/sequence.fa.a3m','data/output_c.a3m')
-
+def run_a3m():
     parse_a3m('data/sequence.fa.sto', 'data/output_p.a3m')
-
-    assert_equal_a3m('data/output_p.a3m', 'data/output_c.a3m')
-    assert_equal_files('data/output_p.a3m', 'data/output_c.a3m')
-
+    cparse_a3m('data/sequence.fa.sto', 'data/output_c.a3m')
     os.unlink('data/output_c.a3m')
     os.unlink('data/output_p.a3m')
 
 
-def test_purepython_fasta():
-    parse_fasta('data/sequence.fa.sto', 'data/output.fasta')
-    assert_equal_a3m('data/sequence.fa.fasta','data/output.fasta')
-    os.unlink('data/output.fasta')
-
-
-def test_cython_fasta():
-    try:
-        from stockholm_reformat import cparse_fasta
-    except ImportError:
-        raise SkipTest('Compiled version unavailable')
-
-    cparse_fasta('data/sequence.fa.sto', 'data/output_c.fasta')
-    assert_equal_a3m('data/sequence.fa.fasta','data/output_c.fasta')
-
+def run_fasta():
     parse_fasta('data/sequence.fa.sto', 'data/output_p.fasta')
-    assert_equal_a3m('data/output_p.fasta', 'data/output_c.fasta')
-    assert_equal_files('data/output_p.fasta', 'data/output_c.fasta')
-
-    os.unlink('data/output_p.fasta')
+    cparse_fasta('data/sequence.fa.sto', 'data/output_c.fasta')
     os.unlink('data/output_c.fasta')
+    os.unlink('data/output_p.fasta')
 
 
-def test_purepython_aln():
-    parse_aln('data/sequence.fa.sto', 'data/output.aln')
-    assert_equal_files('data/sequence.fa.aln','data/output.aln')
-    os.unlink('data/output.aln')
-
-
-def test_cython_aln():
-    try:
-        from stockholm_reformat import cparse_aln
-    except ImportError:
-        raise SkipTest('Compiled version unavailable')
-
-    cparse_aln('data/sequence.fa.sto', 'data/output_c.aln')
-    assert_equal_files('data/sequence.fa.aln','data/output_c.aln')
-
+def run_aln():
     parse_aln('data/sequence.fa.sto', 'data/output_p.aln')
-    assert_equal_files('data/output_p.aln', 'data/output_c.aln')
+    cparse_aln('data/sequence.fa.sto', 'data/output_c.aln')
 
     os.unlink('data/output_p.aln')
     os.unlink('data/output_c.aln')
 
 
 if __name__ == '__main__':
-    #import nose
-    #nose.main()
-
-    g = globals().copy()
-    for f in [obj for name, obj in g.iteritems() if name.startswith('test_')]:
-        f()
+    run_a3m()
+    run_aln()
+    run_fasta()
